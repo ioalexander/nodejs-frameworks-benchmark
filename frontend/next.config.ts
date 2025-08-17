963314b2e1ce416388433e11f654ca43880eaa8a
Alexander <io.alexander.io@pm.me>
2025-08-17 02:12:01 +0200

    cicd

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
+const basePath = process.env.BASE_PATH || "";
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  basePath: isProd ? `/${basePath}` : "",
+  assetPrefix: isProd ? `/${basePath}/` : "",
   output: "export",
   reactStrictMode: true,
   images: { unoptimized: true },
